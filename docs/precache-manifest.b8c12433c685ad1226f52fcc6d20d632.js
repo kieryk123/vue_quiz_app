2fe194fd2ddb0f9611329509e45c2a5e44a5e9f5
Paweł Kieryk <pawel.kieryk@codewise.com>
2018-12-09 01:51:01 +0100

    Change dist to docs

---
+++ docs/precache-manifest.b8c12433c685ad1226f52fcc6d20d632.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "821c42333b41fa487aed613542e8471b",
+    "url": "/media/results_sound.821c4233.mp3"
+  },
+  {
+    "revision": "940f0cce16bfe7c9ecdf8a06d8fc321b",
+    "url": "/media/incorrect_sound.940f0cce.mp3"
+  },
+  {
+    "revision": "7d8dcd4aa2905a22a2d20def5d1f19e1",
+    "url": "/media/correct_sound.7d8dcd4a.mp3"
+  },
+  {
+    "revision": "33519000b55fdf9161ca",
+    "url": "/js/chunk-vendors.a4870015.js"
+  },
+  {
+    "revision": "375f84863a742b2a0295",
+    "url": "/js/app.8f9073ef.js"
+  },
+  {
+    "revision": "214d35ea459f3d9af0d329e18d044516",
+    "url": "/index.html"
+  },
+  {
+    "revision": "375f84863a742b2a0295",
+    "url": "/css/app.29b355a1.css"
+  }
+];
