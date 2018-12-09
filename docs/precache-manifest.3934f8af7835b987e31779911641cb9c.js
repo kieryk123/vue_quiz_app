fa94076e5b5c88ae42e0c2aed084d28bb6c300d6
Paweł Kieryk <pawel.kieryk@codewise.com>
2018-12-09 12:37:33 +0100

    Fix links

--- docs/precache-manifest.3934f8af7835b987e31779911641cb9c.js
+++ docs/precache-manifest.3934f8af7835b987e31779911641cb9c.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = [
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "robots.txt"
   },
   {
     "revision": "821c42333b41fa487aed613542e8471b",
-    "url": "/media/results_sound.821c4233.mp3"
+    "url": "media/results_sound.821c4233.mp3"
   },
   {
     "revision": "940f0cce16bfe7c9ecdf8a06d8fc321b",
-    "url": "/media/incorrect_sound.940f0cce.mp3"
+    "url": "media/incorrect_sound.940f0cce.mp3"
   },
   {
     "revision": "7d8dcd4aa2905a22a2d20def5d1f19e1",
-    "url": "/media/correct_sound.7d8dcd4a.mp3"
+    "url": "media/correct_sound.7d8dcd4a.mp3"
   },
   {
     "revision": "33519000b55fdf9161ca",
-    "url": "/js/chunk-vendors.a4870015.js"
+    "url": "js/chunk-vendors.a4870015.js"
   },
   {
     "revision": "0043b6e9adbdd2c42dcb",
-    "url": "/js/app.0658030b.js"
+    "url": "js/app.0658030b.js"
   },
   {
     "revision": "d87763b6176ddbe831bb1fd3468c1c30",
-    "url": "/index.html"
+    "url": "index.html"
   },
   {
     "revision": "0043b6e9adbdd2c42dcb",
-    "url": "/css/app.29b355a1.css"
+    "url": "css/app.29b355a1.css"
   }
 ];
